ab712461e3167a908790de2be2065fe542843ee4
2e2a <alexej@2e2a.de>
2020-02-18 09:57:54 +0100

    Adjust package info

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import wordpress
 long_description = open('README.rst').read()
 
 setup(
-    name='the-real-django-wordpress',
+    name='the-real-django-wordpress-py3',
     version=wordpress.__version__,
-    description='Django models and views for a WordPress database.',
+    description='Django models and views for a WordPress database (Django 1.11 or later).',
     long_description=long_description,
-    author='Jeremy Carbaugh',
-    author_email='jcarbaugh@sunlightfoundation.com',
-    url='http://github.com/sunlightlabs/django-wordpress/',
+    author='Alexej Starschenko',
+    author_email='alexej@2e2a.de',
+    url='http://github.com/2e2a/django-wordpress/',
     packages=['wordpress'],
     package_data={'wordpress': ['templates/wordpress/*']},
     classifiers=[
